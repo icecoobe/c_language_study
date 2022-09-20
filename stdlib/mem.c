7c94d0af0b96ea207df43f33f41502848499860f
Luke <icecoobe@qq.com>
2022-09-20 18:00:06 +0800

    Create mem.c

---
+++ stdlib/mem.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main(void)
+{
+    char *str;
+ 
+    str = (char *) malloc(15);
+    strcpy(str, "google");
+    printf("String = %s,  Address = %p\n", str, str);
+
+    str = (char *) realloc(str, 25);
+    strcat(str, ".com");
+    printf("String = %s,  Address = %p\n", str, str);
+
+    free(str);
+
+    return 0;
+}
