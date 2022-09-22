2df94ac4bb69b5c25a90d15b147fdc75eb7e88cd
Luke <icecoobe@qq.com>
2022-09-22 15:01:46 +0800

    Update aligned_mem.c
    add hacker's delight version algo.

--- stdlib/aligned_mem.c
+++ stdlib/aligned_mem.c
@@ -12,6 +12,8 @@ static void *my_1st_aligned_alloc(size_t alignment, size_t size);
 static void my_1st_aligned_free(void *mem);
 static void *my_aligned_alloc(size_t alignment, size_t size);
 static void my_aligned_free(void *mem);
+static void *stl_aligned_alloc(size_t alignment, size_t size);
+static void stl_aligned_free(void * aligned_ptr);
 void test(size_t alignment, size_t size);
 
 int main(void)
@@ -47,6 +49,12 @@ int main(void)
            "check alignment: %d\n",
            p, (size_t)p % alignment == 0);
     my_aligned_free(p);
+
+    p = stl_aligned_alloc(alignment, size);
+    printf("STL lib allocated pointer is %p, "
+           "check alignment: %d\n",
+           p, (size_t)p % alignment == 0);
+    stl_aligned_free(p);
 
     return 0;
 }
@@ -141,6 +149,70 @@ my_aligned_free(void *mem)
 
     // free space with raw mem address
     free(praw);
+}
+
+void *
+stl_aligned_alloc(size_t alignment, size_t size)
+{
+    // 分配足够的内存, 这里的算法很经典, 早期的STL中使用的就是这个算法
+
+    // 首先是维护FreeBlock指针占用的内存大小
+    const int pointerSize = sizeof(void*);
+
+    // alignment - 1 + pointerSize这个是FreeBlock内存对齐需要的内存大小
+    // 前面的例子sizeof(T) = 20, __alignof(T) = 16,
+    // g_MaxNumberOfObjectsInPool = 1000
+    // 那么调用本函数就是alignedMalloc(1000 * 20, 16)
+    // 那么alignment - 1 + pointSize = 19
+    const int requestedSize = size + alignment - 1 + pointerSize;
+
+    // 分配的实际大小就是20000 + 19 = 20019
+    void* raw = malloc(requestedSize);
+
+    // 这里实Pool真正为对象实例分配的内存地址
+    uintptr_t start = (uintptr_t)raw + pointerSize;
+    // 向上舍入操作
+    // 解释一下, __ALIGN - 1指明的是实际内存对齐的粒度
+    // 例如__ALIGN = 8时, 我们只需要7就可以实际表示8个数(0~7)
+    // 那么~(__ALIGN - 1)就是进行舍入的粒度
+    // 我们将(bytes) + __ALIGN-1)就是先进行进位, 然后截断
+    // 这就保证了我是向上舍入的
+    // 例如byte = 100, __ALIGN = 8的情况
+    // ~(__ALIGN - 1) = (1 000)B
+    // ((bytes) + __ALIGN-1) = (1 101 011)B
+    // (((bytes) + __ALIGN-1) & ~(__ALIGN - 1)) = (1 101 000 )B = (104)D
+    // 104 / 8 = 13, 这就实现了向上舍入
+    // 对于byte刚好满足内存对齐的情况下, 结果保持byte大小不变
+    // 记得《Hacker's Delight》上面有相关的计算
+    // 这个表达式与下面给出的等价
+    // ((((bytes) + _ALIGN - 1) * _ALIGN) / _ALIGN)
+    // 但是SGI STL使用的方法效率非常高
+    void* aligned = (void*)((start + alignment - 1) & ~(alignment - 1));
+
+    // 这里维护一个指向malloc()真正分配的内存
+    *(void**)((uintptr_t)aligned - pointerSize) = raw;
+
+    printf("pstore=%p, praw=%p\n", *(void**)((uintptr_t)aligned - pointerSize), raw);
+    printf("real size=%llu, offset=%llu\n", requestedSize, (size_t)aligned - (size_t)start);
+
+    // 返回实例对象真正的地址
+    return aligned;
+}
+
+void
+stl_aligned_free(void * aligned_ptr)
+{
+    if (aligned_ptr)
+    {
+        free(((void**)aligned_ptr)[-1]);
+    }
+}
+
+_Bool
+is_aligned(void* data, int alignment)
+{
+    // 又是一个经典算法, 参见<Hacker's Delight>
+    return ((uintptr_t)data & (alignment - 1)) == 0;
 }
 
 void
