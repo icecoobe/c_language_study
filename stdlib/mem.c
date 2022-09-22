7d62eaead592fd6efdd5c7ad4564a0f12102a226
Luke <icecoobe@qq.com>
2022-09-22 16:42:24 +0800

    Update mem.c
    add TODO: mechanism of malloc & free, new & delete

--- stdlib/mem.c
+++ stdlib/mem.c
@@ -4,8 +4,13 @@
 #include <time.h>
 #include <string.h>
 
-// TODO: aligned_alloc demo
-// https://blog.csdn.net/jin739738709/article/details/122992753
+// TODO: 
+// 1.How free() works is more important than malloc(). 
+// https://stackoverflow.com/questions/1119134/how-do-malloc-and-free-work
+// malloc, free/new delete are both os dependent.
+// in general, the heap mem was a linked list, each block is a chunk with
+// meta data stored in previous location before the address malloc returned to user space app.
+// so if there's some wrong operation on pointer, it maybe break the structure of mem management.
 
 int main(void)
 {
